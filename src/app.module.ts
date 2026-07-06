f9f04e28fb32da71971640720c1607e19bec8416
JOAO LUCCAS LORDELO MARQUES <jmarques@COLEGIOPERFIL.COM.BR>
2026-07-06 16:24:28 -0300

    feat: throttler e cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,22 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { CoupleModule } from './couple/couple.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CoupleModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    CoupleModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+    CacheModule.register({
+      isGlobal: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
